c2fe20245f6b19c658d6a5c82aca940f8e659d2e
biandoucheng <mianbizhecy@163.com>
2022-11-04 18:17:17 +0800

    [feat]: 添加go-frequency-ctrl待完成

---
+++ go-frequency-ctrl-example/go.mod
@@ -0,0 +1,5 @@
+module go-frequency-ctrl-example
+
+go 1.18
+
+require github.com/biandoucheng/go-frequency-ctrl v0.0.1-alpha
---
+++ go-frequency-ctrl-example/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	frq "github.com/biandoucheng/go-frequency-ctrl"
+)
+
+func main() {
+	fq := frq.Frequency{}
+	fq.Init(256)
+
+}
