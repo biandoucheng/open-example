13f0901ebfedbd52c550499ac77ccaf311c812ae
biandoucheng <mianbizhecy@163.com>
2023-01-16 17:02:19 +0800

    [feat]: 补充 go-terminator main.go 示例说明

--- go-terminator-example/main.go
+++ go-terminator-example/main.go
@@ -32,3 +32,25 @@ func main() {
 		time.Sleep(time.Second)
 	}
 }
+
+// Go to the root directory of the sample project and execute the packaging command: go build
+// Run ./go-terminator-example
+// Wait a few seconds
+// Press Control + C
+// output:
+/*
+Current Count:  1
+Bang , Bang , Bang
+Current Count:  2
+Current Count:  3
+Current Count:  4
+Bang , Bang , Bang
+Current Count:  5
+^Cgo-terminator: Service interrupted by interrupt signal,and the service is exiting gracefully .
+go-terminator: func globalTerminator.Terminated[0] start running
+.ACountServer is stopping:  2023-01-16 17:00:44.610857 +0800 CST m=+5.340958792
+go-terminator: func globalTerminator.Terminated[0] is done
+.go-terminator: func clockServer.Terminated[1] start running
+.Clock is stopping:  2023-01-16 17:00:44.711366 +0800 CST m=+5.441469334
+go-terminator: func clockServer.Terminated[1] is done
+*/
